da7347726a87cff23f834fd250513825782a0033
Dev Goyal <47804173+Devgoyal2808@users.noreply.github.com>
2021-04-14 10:42:14 +0530

    Add files via upload

---
+++ Database/application1.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2021 at 06:47 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application1`
+--
+
+CREATE TABLE `application1` (
+  `student_id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `application1`
+--
+
+INSERT INTO `application1` (`student_id`, `company_id`) VALUES
+(181112001, 2),
+(181112023, 2),
+(181112081, 2),
+(181112081, 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `application1`
+--
+ALTER TABLE `application1`
+  ADD PRIMARY KEY (`student_id`,`company_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
